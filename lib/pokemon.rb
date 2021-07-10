fa2acc6a30758b5ffbd85567a5c861513114d76b
maxrabin <max.rabin17@gmail.com>
2021-07-10 17:57:59 -0400

    commit tree 2b141d241ddbfb95d899c58190679e73c4c0c7ae

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,31 @@
+require 'pry'
 class Pokemon
+    attr_accessor :id, :name, :type, :db
+
+    def initialize(id: nil, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+    end
+
+    def self.save(name, type, db)
+        sql = <<-SQL
+        INSERT INTO pokemon(name, type)
+        VALUES (?, ?)
+        SQL
+        db.execute(sql, name, type)
+        @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    end
+
+    def self.find(id, db)
+        sql = <<-SQL
+        SELECT *
+        FROM pokemon
+        WHERE id = ?
+        SQL
+        pokemon = db.execute(sql, id).first
+        Pokemon.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], db: db)
+    end
+    # binding.pry
 end
